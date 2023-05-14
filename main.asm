5e9cc8f012588a11db0a99add53634eafdb20725
Clysman <62316222+clys-man@users.noreply.github.com>
2023-05-14 20:24:11 -0300

    showing current cpf in print

--- main.asm
+++ main.asm
@@ -1,5 +1,5 @@
 .data
-	inputFile: .asciiz "/home/clysman/Dev/Unifor/ASM/cpf-validation/test.txt"
+	inputFile: .asciiz "test.txt"
 	validMessage: .asciiz " - Valido \n"
 	invalidMessage: .asciiz " - Invalido \n"
 	newLine: .asciiz "\n"
@@ -13,10 +13,10 @@
   	move $s0, $v0        	# save the file descriptor. $s0 = file
 	
 	#read the file
-	li $v0, 14				# read_file syscall code = 14
-	move $a0, $s0			# file descriptor
-	la $a1, fileWords  		# The buffer that holds the string of the WHOLE file
-	la $a2, 1024			# hardcoded buffer length
+	li $v0, 14		# read_file syscall code = 14
+	move $a0, $s0		# file descriptor
+	la $a1, fileWords  	# The buffer that holds the string of the WHOLE file
+	la $a2, 1024		# hardcoded buffer length
 	syscall
 	
 	# The calculate fucntion use the $t0, $t1, $t2, $t3, $t4, $s1, $s2 registers,
@@ -28,94 +28,103 @@
 	# $t4: result accumulator($t4 = $t3 + $t4)
 	# $s1: lenght of cpf number
 	# $s2: limit of multiplied numbers to calculate validator digit
-	move $t0, $zero			# set 0 to $t0
+	move $t0, $zero				# set 0 to $t0
 	li $t2, 10				# load 10 to initial weight
 	li $s1, 11				# lenght of cpf
 	li $s2, 9				# set 9 to $s2
 	calculate:
 		lb $t1, fileWords($t0)			# load content of file position
-		addu $t1, $t1, -48				# char to int conversion
+		addu $t1, $t1, -48			# char to int conversion
 		
-		beq $t1, -48, exit				# exit when reaching the EOF(-48)
-		beq $t1, -38, jumpToNextCpf 	# call jumpToNextCpf when '\n'(-38)
+		beq $t1, -48, exit			# exit when reaching the EOF(-48)
+		beq $t1, -38, jumpToNextCpf 		# call jumpToNextCpf when '\n'(-38)
 		beq $t0, $s2, calculateValidatorDigit	# call calculateValidatorDigit when $t0(index) equal $s2
 		
-		mul $t3, $t1, $t2				# $t3 = $t1 * $t2
-		add $t4, $t3, $t4				# $t4 = $t3 + $t4
-		sub $t2, $t2, 1					# $t2 = $t2 - 1
-		addi $t0, $t0, 1				# $t0 = $t0 + 1
+		mul $t3, $t1, $t2			# $t3 = $t1 * $t2
+		add $t4, $t3, $t4			# $t4 = $t3 + $t4
+		sub $t2, $t2, 1				# $t2 = $t2 - 1
+		addi $t0, $t0, 1			# $t0 = $t0 + 1
 		
-		j calculate						# return to calculate
+		j calculate				# return to calculate
 	
 	jumpToNextCpf:
-		add $t0, $t0, 1					# when calculate is finish $t0 assume the last index of past cpf number, sum 1 to next
-		li $t2, 10						# set $t2 to initial weight
-		li $s3, 0						# set count of validated digits to zero
-		jal resetRegisters				# reset temporary registers
+		add $t0, $t0, 1				# when calculate is finish $t0 assume the last index of past cpf number, sum 1 to next
+		li $t2, 10				# set $t2 to initial weight
+		li $s3, 0				# set count of validated digits to zero
+		jal resetRegisters			# reset temporary registers
  		
-		addi $s2, $t0, 9				# plus 9 to limit of next cpf				
+		addi $s2, $t0, 9			# plus 9 to limit of next cpf				
 		
-		j calculate						# jump to calculate next cpf
+		j calculate				# jump to calculate next cpf
 	
 	# To calculate the validator digit, the $t5 register
 	# is used to store the valid digit to the current cpf
 	calculateValidatorDigit:
-		div $t4, $s1					# $t4/$s1(11)
-		mfhi $t5						# move rest of division to $t5
+		div $t4, $s1				# $t4/$s1(11)
+		mfhi $t5				# move rest of division to $t5
 		blt $t5, 2, setToZeroValidatorDigit 	# if rest of division is less than two set to 0
 		 
-		sub $t5, $s1, $t5				# $t5 = $s1(11) - $t5
+		sub $t5, $s1, $t5			# $t5 = $s1(11) - $t5
 		
 		j checkValidatorDigit
 		
 	setToZeroValidatorDigit:
-		li $t5, 0						# $t5 = 0
+		li $t5, 0				# $t5 = 0
 		
 		j checkValidatorDigit
 
 	checkValidatorDigit:
 		lb $t6, fileWords($t0)			# load in $t6 the cpf validator digit present in the file
-		addi $t6, $t6, -48				# char to int conversion
+		addi $t6, $t6, -48			# char to int conversion
 		
 		beq $t5, $t6, validCpf			# $t5 == $t6
 		j invalidCpf
 		
 	invalidCpf:
-		jal printCurrentCpf
-		la $a0, invalidMessage
-		jal printMessage
-		
+		add $s4, $t0, -9			# set $s4 to first digit of current cpf
+		add $s5, $s2, 1				# set $s4 to last digit of current cpf
+		jal printCurrentCpf			# print current cpf
+		la $a0, invalidMessage			# load invalid message
+		jal printMessage			# print invalid message
 		j jumpToNextCpf
 	
 	validCpf:
-		addi $t0, $t0, -9		# set $t0 to initial value($t0 = $t0 - 9)
-		addiu $t2, $t2, 10		# set 11 to initial weight($t2 = $t2(1) + 10)
-		addiu $s2, $s2, 1		# set 10 to limit of validator digits($s2 = $s2(9) + 1)
-		addiu $s3, $s3, 1		# number of calculated digits
+		addi $t0, $t0, -9			# set $t0 to initial value($t0 = $t0 - 9)
+		addiu $t2, $t2, 10			# set 11 to initial weight($t2 = $t2(1) + 10)
+		addiu $s2, $s2, 1			# set 10 to limit of validator digits($s2 = $s2(9) + 1)
+		addiu $s3, $s3, 1			# number of calculated digits
 	
-		beq $s3, 2, printValidMessage	# print valid message if number of calculated digits is equals 2
+		beq $s3, 2, printValidMessage		# print valid message if number of calculated digits is equals 2
 		jal resetRegisters
 		j calculate
 	
 	printValidMessage:
-		jal printCurrentCpf
-		la $a0, validMessage
-		jal printMessage
+		add $s4, $t0, -1			# set $s4 to first digit of current cpf
+		add $s5, $s2, -1			# set $s4 to last digit of current cpf
+		jal printCurrentCpf			# print current cpf
+		la $a0, validMessage			# load valid message
+		jal printMessage			# print valid message
 		
 		j jumpToNextCpf
-		
+	
 	printCurrentCpf:
+		lb $a0 fileWords($s4)			# load cpf digit in $a0
+		add $a0, $a0, -48			# char to int conversion
+		li $v0, 1				# print integer
+		syscall
+		add $s4, $s4, 1				# $s4 = $s4 + 1
+		ble $s4, $s5, printCurrentCpf
 		jr $ra
 
 	printMessage:
-		li $v0, 4
+		li $v0, 4				# print char
 		syscall
 		jr $ra
 	
 	resetRegisters:
-		li $t1, 0
-		li $t3, 0
-		li $t4, 0
+		li $t1, 0				# reset read number
+		li $t3, 0				# reset multiplication result
+		li $t4, 0				# reset multiplication accumulator
 		jr $ra
 		
 	exit:
@@ -126,3 +135,4 @@
   		
   		li $v0, 10
   		syscall
+		
